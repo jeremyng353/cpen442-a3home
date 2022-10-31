ac0233bc2e51a11c0dc87f301afba49a03369350
sammauns <maunsellsam@gmail.com>
2022-10-31 16:48:44 -0700

    Co-authored-by: Aswin-SaiSubramanian <Aswin-SaiSubramanian@users.noreply.github.com> Co-authored-by: Harmeeta Dahiya <harmeetadahiya@gmail.com> Co-authored-by: jeremyng353 <jeremyng353@users.noreply.github.com>

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -3,6 +3,7 @@ import secrets
 from cryptography.hazmat.primitives.ciphers import (
     Cipher, algorithms, modes
 )
+import base64 as b64
 from cryptography.hazmat.primitives import padding
 
 # from Assignment3.test import DecryptAndVerifyMessage
@@ -16,32 +17,30 @@ class Protocol:
         self._privateKey = None
         self._g = g
         self._p = p
-        self._message_counter = 1
-        self._nonceA = None
-        self._nonceB = None
+        self._messageCounter = 1
+        self._myNonce = None
+        self._otherNonce = None
         self._myName = name
         self._otherName = None
         self._myExponent = 3 # currently using 3 for testing, replace with secrets.randbits(8)
         self._myDH = (self._g ** self._myExponent) % self._p
         self._otherDH = None
-        self._ga_modp = None
-        self._gb_modp = None
         pass
 
 
     # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
     def GetProtocolInitiationMessage(self):
-        # Message: "App", Ra, 1
+        # Send Message: "my_name", my_nonce, 1
         nonce = secrets.randbits(32)
-        self._nonceA = nonce    
-        self._message_counter = 1
-        return f"{self._myName}, {self._nonceA}, 1"
+        self._myNonce = nonce    
+        self._messageCounter = 1
+        return f"{self._myName}, {self._myNonce}, 1"
 
 
     # Checking if a received message is part of your protocol (called from app.py)
     def IsMessagePartOfProtocol(self, message):
         #check message_counter
-        return int(message.split(", ")[2]) == self._message_counter
+        return int(message.split(", ")[2]) == self._messageCounter
 
 
     # Processing protocol message
@@ -51,73 +50,74 @@ class Protocol:
         args = message.split(", ")
         match args[-1]: # message counter      
             case "1":
-                # “App”, Ra, 1 
+                # Received: “other_name”, other_nonce, 1 
                 self._otherName = args[0]
-                self._nonceA = args[1]
-                print("\n CASE 1 in PROCESS \n")
-                print(f"Other name: {self._otherName}")
-                print(f"Ra = {self._nonceA}")
+                self._otherNonce = args[1]
+                print("\n PROCESSING HANDSHAKE MSG 1 \n")
+                print(f"Other name = {self._otherName}")
+                print(f"Other nonce = {self._otherNonce}")
 
                 # send next msg, increment message_counter
-                self._nonceB = secrets.randbits(32)
-                print(f"Generate Rb = {self._nonceB}")
-                # Rb, E("Sever", Ra, ga mod p, K), 2
-                next_message = f"{self._nonceB}, {self.EncryptAndProtectMessage(f'{self._myName}, {self._nonceA}, {self._myDH}', self._symmetricKey)}, 2"
-                self._message_counter = 2
+                self._myNonce = secrets.randbits(32)
+                print(f"My nonce generated = {self._myNonce}")
+                # my_nonce, E("my_name", other_nonce, myDH, K), 2
+                # TODO vvvvv this line is wrong vvvvv
+                next_message = f"{self._myNonce}, {self.EncryptAndProtectMessage(f'{self._myName}, {self._otherNonce}, {self._myDH}')}, 2"
+                self._messageCounter = 2
                 return next_message
                 
             case "2": # counter = 2
-                ## Rb, E("Sever", Ra, ga mod p, K), 2
-                Rb = args[0]
+                ## Received: other_nonce, E("other_name", my_nonce, otherDH, K), 2
+                self._otherNonce = args[0]
                 cipher_text = args[1]
-                print("\n CASE 2 in PROCESS \n")
+                print("\n PROCESSING HANDSHAKE MSG 2 \n")
                     
                 '''
                     TODO
                     there's something wrong with args[1], i think its due to message being a string instead of being bytes
                     however, aren't messages technically sent as a bitstream anyways
                 '''  
-                plaintext = self.DecryptAndVerifyMessage(cipher_text, self._symmetricKey)
-                other_mode = plaintext[0]
-                Ra = plaintext[1]
-                ga_mod_p = plaintext[2]
+                plaintext = self.DecryptAndVerifyMessage(cipher_text)
+                self._otherName = plaintext[0]
+                my_nonce = plaintext[1]
+                self._otherDH = plaintext[2]
 
-                print(f"Rb = {Rb}")
-                print(f"other name = {other_mode}")
-                print(f"Ra = {Ra}")
-                print(f"g_a mod p = {ga_mod_p}")
+                print(f"other name = {self._otherName}")
+                print(f"My nonce, according to received message = {my_nonce}")
+                print(f"My actual nonce = {self._myNonce}")
+                print(f"Other nonce = {self._otherNonce}")
+                print(f"other DH = {self._otherDH}")
 
-                #assert(Ra == self._nonceA)
+                #assert(my_nonce == self._myNonce)
 
                 # TODO: encryptandprotectmessage is currently byte-like, not string
-                # E(“App”, Rb, gb mod p, K), 3
-                next_message = f"{self.EncryptAndProtectMessage(f'{self._myName}, {self._nonceB}, {self._gb_modp}', self._symmetricKey)}, 3"
+                # Send E(“my_name”, other_nonce, my_DH, K), 3
+                next_message = f"{self.EncryptAndProtectMessage(f'{self._myName}, {self._otherNonce}, {self._myDH}')}, 3"
 
-                self._otherDH = ga_mod_p
-                self._otherDH = 4
                 self.SetSessionKey()
-
-                self._message_counter = 3
+                self._messageCounter = 3
                 return next_message
                 
             case "3":
-                # E(“App”, Rb, gb mod p, K), 3
-                plaintext = self.DecryptAndVerifyMessage(args[0], self._symmetricKey)
-                other_mode = plaintext[0]
-                Rb = plaintext[1]
-                gb_mod_p = plaintext[2]
+                # Received : E(“other_name”, my_nonce, other_DH, K), 3
+                print("\n PROCESSING HANDSHAKE MSG 3 \n")
+                plaintext = self.DecryptAndVerifyMessage(args[0])
+                other_name = plaintext[0]
+                my_nonce = plaintext[1]
+                self._otherDH = plaintext[2]
 
-                print(f"other name = {other_mode}")
-                print(f"Rb = {Rb}")
-                print(f"g_b mod p = {gb_mod_p}")
+                print(f"Other name recieved in encrypted message = {other_name}")
+                print(f"actual other name = {self._otherName}")
+                print(f"my nonce recieved in encrypted message= {my_nonce}")
+                print(f"my actual nonce = {self._myNonce}")
+                print(f"other DH = {self._otherDH}")
 
-                #assert(other_mode == self._otherName)
-                #assert(Rb == self._nonceB)
-                    
-                self._gb_modp = gb_mod_p
+                #assert(other_name == self._otherName)
+                #assert(my_nonce == self._myNonce)
 
                 self.SetSessionKey()
-                self._message_counter = 1       
+                # TODO confirm behavior after connection established
+                self._messageCounter = 1       
                 #TODO: send message  with DH key  
                 next_message = "encrypted data"
                 return next_message
@@ -136,26 +136,36 @@ class Protocol:
     # Encrypting messages
     # TODO: IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERIFICATION OR AUTHENTICATION FAILS
-    def EncryptAndProtectMessage(self, plaintext, key):
+    def EncryptAndProtectMessage(self, plaintext):
         # TODO: use key as key can be DH, shared secret symmetric_key
         iv = os.urandom(16)      
-        return self._Encrypt(plaintext, self._symmetricKey, iv)
+        encrypted = self._Encrypt(plaintext, self._symmetricKey, iv)
+        print(f"type of the encrypted message = {type(encrypted)}")
+        return encrypted
 
-
-    # Decrypting and verifying messages
+    # Decrypting and verifying messages, specifically for processing 2nd and 3rd messages in handshake.
     # TODO: IMPLEMENT DECRYPTION AND INTEGRITY CHECK WITH THE SESSION KEY
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERIFICATION OR AUTHENTICATION FAILS
-    def DecryptAndVerifyMessage(self, ciphertext, key):
+    def DecryptAndVerifyMessage(self, ciphertext):
         # TODO: use key because it can be DH, symmetric_key
-        return ["Server", "Ra", "g_a mod p"]
         # ciphertext = str(ciphertext).encode()
+        print(type(ciphertext))
+        
         iv = ciphertext[0:16]
         tag = ciphertext[-16:]
-        decryptor = Cipher(algorithms.AES(self._symmetricKey), modes.CBC(iv)).decryptor()
+        print("-------------------------")
+        print(ciphertext)
+        print(iv)
+        print(b64.b64encode(iv.encode("utf-8")))
+        print("-------------------------")
+        decryptor = Cipher(algorithms.AES(self._symmetricKey), modes.CBC(b64.b64encode(iv.encode("utf-8")))).decryptor()
         unpadder = padding.PKCS7(128).unpadder()
-        plaintext = decryptor.update(ciphertext[16:]) + decryptor.finalize()
+        # don't decrypt first 16 and last 16 bytes since they're iv and MAC
+        plaintext = decryptor.update(b64.b64encode(ciphertext[16:len(ciphertext)-16].encode("utf-8"))) + decryptor.finalize()
         plaintext = unpadder.update(plaintext) + unpadder.finalize()
         plaintext = plaintext.decode('utf-8')
+        print("PLAINTEXT")
+        print(plaintext)
         
         tag_new = self._Encrypt(plaintext, self._symmetricKey, iv)[-16:]
         if tag != tag_new:
@@ -170,7 +180,6 @@ class Protocol:
             return "ERROR: authentication check failed"
 
     def _Encrypt(self, plaintext, key, iv):
-        return "Cipher text"
         encryptor = Cipher(algorithms.AES(key), modes.CBC(iv)).encryptor()
         padder = padding.PKCS7(128).padder()
         padded_data = padder.update(bytes(plaintext, 'utf-8')) + padder.finalize()
@@ -182,13 +191,17 @@ class Protocol:
         # TODO: needs to return as a string
         # retval = iv + encryptor.update(padded_data) + encryptor.finalize()
         # return int.from_bytes(retval, "big")
-        return iv + encryptor.update(padded_data) + encryptor.finalize()
+        ciphertext = iv + encryptor.update(padded_data) + encryptor.finalize()
+        # attach MAC in plaintext
+        ciphertext += ciphertext[-16:]
+        return ciphertext
+        # return iv + encryptor.update(padded_data) + encryptor.finalize()
 
 
 # Main logic
 if __name__ == '__main__':
-    # g=3, p=5, K=10 
-    prot = Protocol("Client", 10, 3, 5)
+    # g=3, p=5, K=os.urandom(16) 
+    prot = Protocol("Client", os.urandom(16), 3, 5)
     init_msg = prot.GetProtocolInitiationMessage()
     print(f"Initial message = {init_msg}")
     check = prot.IsMessagePartOfProtocol(init_msg)
